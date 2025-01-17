176bb489b9803bccb54e1eff0140c48b622f2575
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2025-01-17 09:22:31 +0200

    updated routes

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"tribe-payments-wallet-golang-interview-assignment/internal/api/httpv1"
+	"tribe-payments-wallet-golang-interview-assignment/internal/wallet"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sumup-oss/go-pkgs/logger"
@@ -10,10 +11,15 @@ import (
 func RegisterRoutes(
 	mux *chi.Mux,
 	log logger.StructuredLogger,
+	walletService *wallet.WalletService,
 ) {
 	mux.Get("/live", Health)
 
 	mux.Route("/v1", func(r chi.Router) {
-		r.Post("/wallet", httpv1.NewCreateWalletHandler(log))
+		r.Post("/wallet/{id}/withdraw", httpv1.WithdrawFromWalletHandler(log, walletService))
+		r.Post("/wallet/{id}/deposit", httpv1.DepositInWalletHandler(log, walletService))
+		r.Get("/wallet/{id}", httpv1.GetWalletHandler(log, walletService))
+		r.Post("/wallet", httpv1.CreateWalletHandler(log, walletService))
 	})
+
 }
